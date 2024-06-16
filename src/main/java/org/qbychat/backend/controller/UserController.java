073ad573e419cd52b7f0e7d61e00d6151daab0b2
cubewhy <me@lunarclient.top>
2024-06-16 19:36:27 +0800

    Marge RegisterController.java to UserController.java

--- src/main/java/org/qbychat/backend/controller/UserController.java
+++ src/main/java/org/qbychat/backend/controller/UserController.java
@@ -4,17 +4,19 @@ import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.log4j.Log4j2;
 import lombok.val;
+import org.qbychat.backend.entity.Account;
 import org.qbychat.backend.entity.RestBean;
+import org.qbychat.backend.service.impl.AccountServiceImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 @Log4j2
 public class UserController {
+    @Resource
+    private AccountServiceImpl accountService;
+
     @Resource
     BCryptPasswordEncoder passwordEncoder;
 
@@ -23,11 +25,15 @@ public class UserController {
         return RestBean.success(request.getUserPrincipal().getName());
     }
 
-    @PostMapping("register")
-    public RestBean<Object> register(HttpServletRequest request) {
-        String username = request.getParameter("username");
-        String email = request.getParameter("email");
-        // TODO register
-        return RestBean.success();
+    @PostMapping("/register")
+    public RestBean<String> registerUser(@RequestParam Account account) {
+        if (accountService.findAccountByNameOrEmail(account.getUsername()) == null) {
+            return RestBean.failure(401, "User exist.");
+        }
+        if (accountService.findAccountByNameOrEmail(account.getEmail()) == null) {
+            return RestBean.failure(401, "Email exist.");
+        }
+        accountService.registerAccount(account);
+        return RestBean.success("User registered.");
     }
 }
