07db8e38dbacfb73b61ffbb4311c0655415eb51a
zszfympx <2792668017@qq.com>
2024-06-22 19:43:59 +0800

    Try fix mailsenderservice by bean.

--- src/main/java/org/qbychat/backend/config/MailConfig.java
+++ src/main/java/org/qbychat/backend/config/MailConfig.java
@@ -1,28 +1,32 @@
 package org.qbychat.backend.config;
 
+import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
 
 @Configuration
 public class MailConfig {
-    @Bean
-    public JavaMailSenderImpl javaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp-mail.outlook.com");
-//        mailSender.setPort(25);
+    @Resource
+    private Environment env;
 
-        mailSender.setUsername("zxysb1145@outlook.com");
-        mailSender.setPassword("qwer1145");
-        mailSender.setDefaultEncoding("UTF-8");
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(env.getProperty("spring.mail.host"));
+        mailSender.setPort(Integer.parseInt(env.getProperty("spring.mail.port")));
+        mailSender.setUsername(env.getProperty("spring.mail.username"));
+        mailSender.setPassword(env.getProperty("spring.mail.password"));
 
         Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.transport.protocol", env.getProperty("spring.mail.protocol"));
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-//        props.put("mail.debug", "true");
+
         return mailSender;
     }
 }
