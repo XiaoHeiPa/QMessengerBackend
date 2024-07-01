328ec3706129c7505bd92075a3d70e1370230ed3
cubewhy <cubewhy@163.com>
2024-07-01 16:55:54 +0800

    untested features (message)

--- src/main/java/org/qbychat/backend/service/MessageService.java
+++ src/main/java/org/qbychat/backend/service/MessageService.java
@@ -1,4 +1,12 @@
 package org.qbychat.backend.service;
 
+import org.qbychat.backend.entity.ChatMessage;
+
+import java.util.List;
+
 public interface MessageService {
+    void addMessage(ChatMessage message);
+    void removeMessage(ChatMessage message);
+
+    List<ChatMessage> getMessages(Integer channel, boolean isGroup);
 }
--- src/main/java/org/qbychat/backend/service/impl/MessageServiceImpl.java
+++ src/main/java/org/qbychat/backend/service/impl/MessageServiceImpl.java
@@ -1,12 +1,34 @@
 package org.qbychat.backend.service.impl;
 
+import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.spring.service.impl.ServiceImpl;
 import org.qbychat.backend.entity.ChatMessage;
 import org.qbychat.backend.mapper.MessageMapper;
 import org.qbychat.backend.service.MessageService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+import static org.qbychat.backend.entity.table.MessagesTableDef.MESSAGES;
+
 @Service
 public class MessageServiceImpl extends ServiceImpl<MessageMapper, ChatMessage> implements MessageService {
-    // TODO
+    @Override
+    public void addMessage(ChatMessage message) {
+        this.mapper.insert(message);
+    }
+
+    @Override
+    public void removeMessage(ChatMessage message) {
+        this.mapper.delete(message);
+    }
+
+    @Override
+    public List<ChatMessage> getMessages(Integer channel, boolean isDirectMessage) {
+        QueryWrapper qw = new QueryWrapper();
+        qw.select(MESSAGES.ALL_COLUMNS)
+                .where(MESSAGES.ID.eq(channel))
+                .and(MESSAGES.IS_DM.eq(isDirectMessage));
+        return this.mapper.selectListByQuery(qw);
+    }
 }
