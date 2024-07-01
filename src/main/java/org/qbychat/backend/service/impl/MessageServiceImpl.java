a111361c2d2f0ea5556485050911f734ea5e4b58
cubewhy <cubewhy@163.com>
2024-07-01 19:40:17 +0800

    change pageSize to 100

--- src/main/java/org/qbychat/backend/service/impl/MessageServiceImpl.java
+++ src/main/java/org/qbychat/backend/service/impl/MessageServiceImpl.java
@@ -5,14 +5,22 @@ import com.mybatisflex.spring.service.impl.ServiceImpl;
 import org.qbychat.backend.entity.ChatMessage;
 import org.qbychat.backend.mapper.MessageMapper;
 import org.qbychat.backend.service.MessageService;
+import org.springframework.boot.actuate.autoconfigure.data.redis.RedisHealthContributorAutoConfiguration;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.qbychat.backend.entity.table.MessagesTableDef.MESSAGES;
 
 @Service
 public class MessageServiceImpl extends ServiceImpl<MessageMapper, ChatMessage> implements MessageService {
+    private final RedisHealthContributorAutoConfiguration redisHealthContributorAutoConfiguration;
+
+    public MessageServiceImpl(RedisHealthContributorAutoConfiguration redisHealthContributorAutoConfiguration) {
+        this.redisHealthContributorAutoConfiguration = redisHealthContributorAutoConfiguration;
+    }
+
     @Override
     public void addMessage(ChatMessage message) {
         this.mapper.insert(message);
@@ -29,6 +37,11 @@ public class MessageServiceImpl extends ServiceImpl<MessageMapper, ChatMessage> 
         qw.select(MESSAGES.ALL_COLUMNS)
                 .where(MESSAGES.TO.eq(channel))
                 .where(MESSAGES.IS_DM.eq(isDirectMessage));
-        return this.mapper.paginate(this.mapper.selectListByQuery(qw).size(), 2, qw).getRecords();
+        int size = this.mapper.selectListByQuery(qw).size();
+        int pageSize = 100;
+        List<ChatMessage> records = new ArrayList<>(this.mapper.paginate(size - 1, pageSize, qw)
+                .getRecords());
+        records.addAll(this.mapper.paginate(size - 2, pageSize, qw).getRecords());
+        return records;
     }
 }
