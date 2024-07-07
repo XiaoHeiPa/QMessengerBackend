479b263cfe9330a5f9763c9e64e3a61996388161
cubewhy <cubewhy@163.com>
2024-07-08 07:14:28 +0800

    param arg

--- src/main/java/org/qbychat/backend/controller/AvatarController.java
+++ src/main/java/org/qbychat/backend/controller/AvatarController.java
@@ -3,10 +3,7 @@ package org.qbychat.backend.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.apache.commons.io.IOUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -17,7 +14,7 @@ import static org.qbychat.backend.QMessengerBackendApplication.CONFIG_DIR;
 @RequestMapping("/avatar")
 public class AvatarController {
     @GetMapping("query")
-    public void query(HttpServletRequest request, HttpServletResponse response, @RequestBody Integer id, @RequestBody int isUser) throws Exception {
+    public void query(HttpServletRequest request, HttpServletResponse response, @RequestParam Integer id, @RequestParam int isUser) throws Exception {
         File avatarFile = new File(CONFIG_DIR, "avatar/" + (isUser == 1 ? "users" : "groups") + "/" + id + ".png");
         if (!avatarFile.exists()) {
             response.sendRedirect("https://http.cat/404.png");
