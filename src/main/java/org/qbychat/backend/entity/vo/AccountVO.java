f544015070119e4f9b62ebfea5343d306bff76e5
cubewhy <cubewhy@163.com>
2024-07-14 22:12:52 +0800

    fix

--- src/main/java/org/qbychat/backend/entity/vo/AccountVO.java
+++ src/main/java/org/qbychat/backend/entity/vo/AccountVO.java
@@ -1,6 +1,7 @@
 package org.qbychat.backend.entity.vo;
 
 import lombok.Data;
+import org.qbychat.backend.entity.Role;
 
 import java.util.Date;
 
@@ -9,7 +10,7 @@ public class AccountVO {
     private Integer id;
     private String username;
     private String email;
-    private String role;
+    private Role role;
     private Long registerTime;
     private String nickname;
 }
